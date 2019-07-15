2c76fbaa575c529cb24cbe434790a92f7332306c
ib_admin <mohan145>
2019-07-15 09:55:13 +0530

    commited after writing tests for get_posts_with_more_positive_reactions()

--- fbpost/test_dir/test_post_methods.py
+++ fbpost/test_dir/test_post_methods.py
@@ -1,6 +1,7 @@
 import pytest
 from postv1.models import User, Post, Reaction, Comment
-from postv1.model_methods import create_post, get_post, react_to_post, ReactionType, get_reaction_metrics
+from postv1.model_methods import get_posts_with_more_positive_reactions, create_post, get_post, react_to_post, \
+    ReactionType, get_reaction_metrics
 
 
 def create_user(name, url):
@@ -29,11 +30,15 @@ def create_post_data(content, uname, url):
 def user_setup():
     # User.objects.create(name='user1', profile_pic_url="user1@xyz.com")
     create_user(name='user1', url='user1@xyz.com')
+    create_user(name='user2', url='user1@xyz.com')
+    create_user(name='user3', url='user1@xyz.com')
 
 
 @pytest.fixture
 def post_setup():
     create_post_data(content='post1', uname='user1', url='user1@xyz.com')
+    create_post_data(content='post2', uname='user2', url='user2@xyz.com')
+    create_post_data(content='post3', uname='user3', url='user3@xyz.com')
 
 
 # create post test
@@ -162,7 +167,7 @@ def test_react_to_post_user_corresponding_post_change_reaction():
 @pytest.mark.django_db
 def test_reaction_metrics_post_exists(post_setup):
     with pytest.raises(Exception) as e:
-        get_reaction_metrics(post_id=2)
+        get_reaction_metrics(post_id=9)
     assert "Post does not exist" in str(e.value)
 
 
@@ -180,5 +185,34 @@ def test_post_reaction_metrics_metrics_data(post_setup, user_setup):
     assert {'reaction': ReactionType.HAHA.value, 'count': 1} in res
     assert {'reaction': ReactionType.LIKE.value, 'count': 2} in res
     assert {'reaction': ReactionType.SAD.value, 'count': 1} in res
+
+
+@pytest.mark.django_db
+def test_post_with_more_pos_reactions(post_setup, user_setup):
+    print(Post.objects.all())
+    post1 = Post.objects.get(id=1)
+    post2 = Post.objects.get(id=2)
+    post3 = Post.objects.get(id=3)
+
+    user1 = post1.posted_by
+    user2 = post2.posted_by
+    user3 = post3.posted_by
+
+    # more positive reactions
+    post1.reactions.create(user=user3, reaction=ReactionType.WOW.value)
+    post1.reactions.create(user=user2, reaction=ReactionType.SAD.value)
+
+    # equal pos and neg reactions
+    post2.reactions.create(user=user1, reaction=ReactionType.WOW.value)
+    post2.reactions.create(user=user2, reaction=ReactionType.SAD.value)
+    post2.reactions.create(user=user3, reaction=ReactionType.ANGRY.value)
+
+    # more neg reactions
+    post3.reactions.create(user=user2, reaction=ReactionType.ANGRY.value)
+    post3.reactions.create(user=user3, reaction=ReactionType.SAD.value)
+
+    result = get_posts_with_more_positive_reactions()
+
+    assert Post.objects.get(id=result[0]) == post1
 
 
